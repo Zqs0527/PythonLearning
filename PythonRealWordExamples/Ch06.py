341bc76a18036e4197b78ecaf5a64c9629537e5f
Yuan Chen <ccyyzqs@gmail.com>
2022-06-29 22:52:37 +0200

    Stacks

--- PythonRealWordExamples/Ch06.py
+++ PythonRealWordExamples/Ch06.py
@@ -7,3 +7,10 @@ print(q.empty())
 q.put('bag2')
 q.put('bag3')
 print(q.get())
+
+
+stack = list()
+stack.append('bill1')
+stack.append('bill2')
+bill_type = stack.pop() # this will return 'bill2', because it is last in
+print(bill_type)
